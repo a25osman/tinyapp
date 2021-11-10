fd62e4fb7c01e7fa33ab8f992ba677532ecbbfec
Awab Osman <awabosman3@gmail.com>
2021-11-09 21:37:41 -0500

    url shortening

--- express_server.js
+++ express_server.js
@@ -16,7 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const code = generateRandomString()
+  urlDatabase[code] = req.body.longURL;
+  res.redirect(`/urls/${code}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
@@ -31,6 +33,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 
